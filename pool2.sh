a8a95ce4b00cb4bf1be4b44f630e325f41f2ef03
Mihail Konev <k.mvc@ya.ru>
2016-09-14 01:45:13 +0000

    user manages job io redirection
    Move " 2>&1 > job_output_NAME " logic to inlinings.

--- pool2.sh
+++ pool2.sh
@@ -18,7 +18,7 @@ job_spawn() {
 
   eval "job_done_$name=r"
   rm "job_status_$name" 2>/dev/null
-  eval "job_self=\"$name\" job_spawn_run $func 2>&1 > job_output_$name &"
+  eval "job_self=\"$name\" job_spawn_run $func &"
 }
 
 job_spawn_run() {
@@ -63,18 +63,10 @@ job_is_done() {
   return 1
 }
 
-job_report() {
-  local name="$1" ; shift
-
-  echo "Output of job $name : {{"
-  cat "job_output_$name"
-  echo "}}"
-}
-
 job_cleanup() {
   local name="$1" ; shift
 
-  rm "job_output_$name" "job_status_$name" 2>/dev/null || true
+  rm "job_status_$name" 2>/dev/null || true
 }
 
 work_n() {
@@ -85,13 +77,21 @@ work_n() {
   echo end job "$n"
 }
 
-work1() { job_spawn 1 "work_n 1"; }
-work2() { job_spawn 2 "work_n 2"; }
+work_report() {
+  local name="$1" ; shift
+
+  echo "Output of work $name : {{"
+  cat "work_output_$name"
+  echo "}}"
+}
+
+work1() { job_spawn 1 "work_n 1 2>&1 > work_output_1"; }
+work2() { job_spawn 2 "work_n 2 2>&1 > work_output_2"; }
 
 work3_work4() {
   if job_is_done 1 && job_is_done 2 ; then
-    job_spawn 3 "work_n 3"
-    job_spawn 4 "work_n 4"
+    job_spawn 3 "work_n 3 2>&1 > work_output_3"
+    job_spawn 4 "work_n 4 2>&1 > work_output_4"
   fi
 }
 
@@ -104,6 +104,7 @@ work_fail() {
 cleanup='
 for job in 1 2 3 4 test_fail noexist
 do job_cleanup "$job"
+   rm "work_output_$job" 2>/dev/null
 done
 '
 
@@ -114,20 +115,20 @@ while true
 do
   job_alldone=y
 
-  job_check 1 work1 "job_report 1"
-  job_check 2 work2 "job_report 2"
-  job_check 3 work3_work4 "job_report 3"
-  job_check 4 work3_work4 "job_report 4"
+  job_check 1 work1 "work_report 1"
+  job_check 2 work2 "work_report 2"
+  job_check 3 work3_work4 "work_report 3"
+  job_check 4 work3_work4 "work_report 4"
 
   job_check test_fail \
-    "job_spawn test_fail work_fail" \
-    "job_report test_fail; echo; echo test_fail ok unexpectedly.; echo" \
-    "job_report test_fail; echo; echo test_fail failed as expected.; echo"
+    "job_spawn test_fail 'work_fail 2>&1 > work_output_test_fail'" \
+    "work_report test_fail; echo; echo test_fail ok unexpectedly.; echo" \
+    "work_report test_fail; echo; echo test_fail failed as expected.; echo"
 
   job_check noexist \
     "job_spawn  noexist work_noexist" \
-    "job_report noexist; echo; echo noexist ok unexpectedly.; echo" \
-    "job_report noexist; echo; echo noexist failed as expected.; echo"
+    "work_report noexist; echo; echo noexist ok unexpectedly.; echo" \
+    "work_report noexist; echo; echo noexist failed as expected.; echo"
 
   if test _"$job_alldone" = _y ; then
     break
